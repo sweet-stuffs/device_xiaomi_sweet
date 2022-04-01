d1ceb7e4543f8f10edd9d7a5d238e93268d684b1
K R I S H N A <krishnakripa34567@gmail.com>
2022-04-01 12:04:55 -0400

    sweet: Adapt to PixelOS
    Change-Id: Ib019186b5b3f5d013098e40d37cee3572fdc893a
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

--- custom_sweet.mk
+++ custom_sweet.mk
@@ -12,10 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+TARGET_SCREEN_WIDTH := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NAME := custom_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
